c80f75ba3addd12ac42b1acb2a4ccc35cb367824
shirase <shirase55@gmail.com>
2016-11-15 22:46:16 +0300

    Models, ArrayHelper

---
+++ src/ArrayHelper.php
@@ -0,0 +1,27 @@
+<?php
+namespace shirase\helpers;
+
+class ArrayHelper
+{
+    public static function normalize(&$data) {
+        if(is_array($data) && sizeof($data)) {
+            list(, $val) = each($data);
+            if(is_array($val) && array_key_exists(0, $val)) {
+                $m = array();
+                foreach(array_keys($data) as $field) {
+                    foreach($data[$field] as $i=>$val) {
+                        $m[$i][$field] = $val;
+                    }
+                }
+                $data = $m;
+            } else {
+                foreach(array_keys($data) as $key) {
+                    self::normalize($data[$key]);
+                }
+                if (is_array($data) && !array_key_exists(0, $data)) {
+                    self::normalize($data);
+                }
+            }
+        }
+    }
+}
---
+++ src/Models.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace shirase\yii2\helpers;
+
+use yii\base\Object;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+
+class Models extends Object
+{
+    /**
+     * @param ActiveRecord $baseModel
+     * @param array $data
+     * @param null $formName
+     * @return array|null
+     * @throws \yii\db\Exception
+     */
+    public static function load($baseModel, $data, $formName = null) {
+        $scope = $formName === null ? $baseModel->formName() : $formName;
+        if (isset($data[$scope])) {
+            $rows = ArrayHelper::getValue($data, $scope, []);
+
+            \common\components\helpers\ArrayHelper::normalize($rows);
+            $tx = \Yii::$app->db->beginTransaction();
+            $ok = true;
+            foreach ($rows as $i=>$row) {
+                if ($row['id']) {
+                    $model = $baseModel::findOne($row['id']);
+                } else {
+                    $model = clone $baseModel;
+                }
+
+                $model->attributes = $row;
+                $rows[$i] = $model;
+                if (!$model->save()) {
+                    $ok = false;
+                }
+            }
+            if ($ok) {
+                $tx->commit();
+                return $rows;
+            } else {
+                $tx->rollBack();
+            }
+        }
+
+        return null;
+    }
+}

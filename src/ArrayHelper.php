24003eb30b1414be70554ba8397bd13af3f6886d
shirase55 <shirase55@gmail.com>
2017-01-19 09:51:55 +0300

    ArrayHelper normalize model[]field

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -5,6 +5,20 @@ class ArrayHelper extends \yii\helpers\ArrayHelper
 {
     public static function normalize(&$data) {
         if(is_array($data) && sizeof($data)) {
+            if(array_key_exists(0, $data) && is_array($data[0])) {
+                // model[]field
+                $corrected = [];
+                foreach ($data as $i=>$val) {
+                    list($k, $v) = each($val);
+                    if (!isset($corrected[$k])) {
+                        $corrected[$k] = [];
+                    }
+                    $corrected[$k][] = $v;
+                }
+                $data = $corrected;
+                reset($data);
+            }
+
             list(, $val) = each($data);
             if(is_array($val) && array_key_exists(0, $val)) {
                 $m = array();
@@ -15,6 +29,7 @@ class ArrayHelper extends \yii\helpers\ArrayHelper
                 }
                 $data = $m;
             } else {
+                // name[model][field][]
                 foreach(array_keys($data) as $key) {
                     self::normalize($data[$key]);
                 }
